397759aa3a79d324a8035d62caf5216c42d438f9
Ravi-Garg <ravi.garg@adelaide.edu.au>
2017-02-06 11:55:16 +1030

    Update train_resnet_50by2_pool_tvl1.sh

--- scripts/train_resnet_50by2_pool_tvl1.sh
+++ scripts/train_resnet_50by2_pool_tvl1.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
-TOOLS=/home/saroj/caffe_new/caffe/build/tools
+TOOLS=../../../build/tools
 
 $TOOLS/caffe train\
   --solver=solver_resnet50by2_pooladam_tvl1.prototxt\
   --gpu=0,1,2,3\
   --log_dir=logs/
+  
+  # run it from caffe/matlab/demo folder
